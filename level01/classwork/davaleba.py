9279f71fc6d884227fd1e21aafd51e7c740b354b
sandroiremadze <maia.jorbenadze1994@gmail.com>
2025-07-28 14:56:32 +0400

    first commit

---
+++ level01/classwork/davaleba.py
@@ -0,0 +1,17 @@
+saxeli="sandro"
+
+asaki=9
+
+
+
+
+gvari="iremadze" 
+
+
+print(asaki)
+
+print(gvari)
+
+print(saxeli)
+
+
---
+++ level02/classwork/davaleba.py
@@ -0,0 +1,12 @@
+
+print("hello")
+
+name = "Aleksandre"
+print(name)
+
+kibatono = 123
+print(kibatono)
+
+hello = 123
+print(hello)
+
